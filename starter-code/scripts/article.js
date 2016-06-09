e33faa612e682e63479a386aeab702944cf431ab
Code Fellows <codefellows@Stevens-MacBook-Pro.local>
2016-06-09 13:21:16 -0700

    finished all the TODOS

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -13,7 +13,8 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles!
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced
@@ -24,6 +25,7 @@ Article.prototype.toHtml = function() {
 
   // TODO: Use the function that Handlebars gave you to return your filled-in
   //       html template for THIS article.
+  return template(this);
 };
 
 ourLocalData.sort(function(a,b) {
